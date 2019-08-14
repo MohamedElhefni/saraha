060e01b33a3f1679855e1e47f53fc06af2701e12
Mohamed Hossam <elhefnymohamed446@gmail.com>
2019-08-14 08:37:23 +0200

    work without any errors  and without csrf prot

--- register.php
+++ register.php
@@ -34,7 +34,6 @@ require 'core/init.php';
                         <label for="name">name</label>
                         <input type="text" name="name" id="name" value="<?php echo  escape(input::get('name')) ?>" placeholder="Enter name">
                     </div>
-                    <input type="hidden" name="token" value="<?php echo token::generate() ?>">
                     <input type="submit" value="Register">
                 </form>
             </div>
@@ -46,48 +45,46 @@ require 'core/init.php';
     <?php
 
     if (input::exists()) {
-        if (token::check(input::get('token'))) {
-            $validate = new validate();
-            $validation = $validate->check($_POST, array(
-                'username'      => array(
-                    'required'  => true,
-                    'min'   => 2,
-                    'max'   => 20,
-                    'unique' => 'users'
-                ),
-                'password'      => array(
-                    'required'  => true,
-                    'min'   => 6,
-                ),
-                'password_again'      => array(
-                    'required'  => true,
-                    'min'   => 6,
-                    'matches' => 'password'
+        $validate = new validate();
+        $validation = $validate->check($_POST, array(
+            'username'      => array(
+                'required'  => true,
+                'min'   => 2,
+                'max'   => 20,
+                'unique' => 'users'
+            ),
+            'password'      => array(
+                'required'  => true,
+                'min'   => 6,
+            ),
+            'password_again'      => array(
+                'required'  => true,
+                'min'   => 6,
+                'matches' => 'password'
 
-                ),
-                'name'      => array(
-                    'required'  => true,
-                    'min'   => 2,
-                    'max'   => 50,
-                ),
-            ));
-            if ($validation->passed()) {
-                echo "
+            ),
+            'name'      => array(
+                'required'  => true,
+                'min'   => 2,
+                'max'   => 50,
+            ),
+        ));
+        if ($validation->passed()) {
+            echo "
             <script>
                 success();
             </script>
         ";
-            } else {
-                $err = '';
-                foreach ($validation->errors() as $valid) {
-                    $err .= $valid . '<br>';
-                }
-                echo "
+        } else {
+            $err = '';
+            foreach ($validation->errors() as $valid) {
+                $err .= $valid . '<br>';
+            }
+            echo "
             <script>
                 error('$err')
             </script>
         ";
-            }
         }
     }
     ?>
