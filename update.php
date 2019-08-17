1856628fcacba9aeaf6909140b840687e1d7b6f8
Mohamed Hossam <elhefnymohamed446@gmail.com>
2019-08-17 03:22:52 +0200

    upload

--- update.php
+++ update.php
@@ -87,7 +87,7 @@ if (!$user->isloggedIn()) {
 
                             <div class="flex items-center justify-between">
                                 <button type="submit" class="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded">
-                                    Update
+                                    Update Profile
                                 </button>
 
 
