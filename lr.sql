1e3d65f9e4c713dfa513c5a7263e65044a6aa098
Mohamed Hossam <elhefnymohamed446@gmail.com>
2019-08-17 11:53:14 +0200

    upload data base

---
+++ lr.sql
@@ -0,0 +1,191 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 17, 2019 at 11:52 AM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lr`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups`
+--
+
+CREATE TABLE `groups` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `permissions` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `groups`
+--
+
+INSERT INTO `groups` (`id`, `name`, `permissions`) VALUES
+(1, 'Standerd User', ''),
+(2, 'Adminstartion', '{\"admin\": 1}');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `id`
+--
+
+CREATE TABLE `id` (
+  `id` int(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `id`
+--
+
+INSERT INTO `id` (`id`) VALUES
+(1),
+(2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `message_id` int(100) NOT NULL,
+  `to_user_id` varchar(32) NOT NULL,
+  `message` text CHARACTER SET utf8 NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`message_id`, `to_user_id`, `message`, `date`) VALUES
+(1, 'c4ca4238a0b923820dcc509a6f75849b', 'How are you mohamed ?? \r\n', '2019-08-17 08:28:33'),
+(2, 'c81e728d9d4c2f636f067f89cc14862c', 'Your Are The Best Teacher Ever \r\nI learned every thing from you \r\nyou are my leader and hope one day to meet YOU :) \r\nBEST WISHES', '2019-08-17 08:29:39');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` varchar(32) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `salt` varchar(32) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `joined` datetime NOT NULL,
+  `group` int(11) NOT NULL,
+  `avatar` varchar(100) NOT NULL DEFAULT 'https://ryo628matsumoto.sarahah.com/img/avatar.png'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `salt`, `name`, `joined`, `group`, `avatar`) VALUES
+('c4ca4238a0b923820dcc509a6f75849b', 'mohamed', 'e129e193b230f944a8fcd2087188d670ff242363ebafa4b38d26524c85eea994', 'sAœg¾×-…ÆÛ¥Ö‡äîêŒêz‡Ž', 'Mohamed Hossam Salah', '2019-08-17 10:24:05', 1, 'uploads/rami.jpg'),
+('c81e728d9d4c2f636f067f89cc14862c', 'Osama', 'c0104940dd478c55aeedc997408afd8608bdbfe1339ab412722219bb992733d9', 'ì··y3¡RW2ÜJ	i†ÐÍÛlèìLÍc\Zß/ðC’r', 'Osama Mohamed', '2019-08-17 10:27:50', 1, 'uploads/osama.jpg');
+
+--
+-- Triggers `users`
+--
+DELIMITER $$
+CREATE TRIGGER `tg_tbi_table1` BEFORE INSERT ON `users` FOR EACH ROW BEGIN
+  INSERT INTO id () VALUES ();
+  SET NEW.id = MD5(LAST_INSERT_ID());
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_sessoin`
+--
+
+CREATE TABLE `users_sessoin` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `hash` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `id`
+--
+ALTER TABLE `id`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`message_id`);
+
+--
+-- Indexes for table `users_sessoin`
+--
+ALTER TABLE `users_sessoin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `id`
+--
+ALTER TABLE `id`
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `message_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users_sessoin`
+--
+ALTER TABLE `users_sessoin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
